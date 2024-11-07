cfbd7e7300e91740e1b270ae966e2d4cdf4652e1
dasol <5dasoll37@gmail.com>
2024-11-07 23:51:19 +0900

    feat: 모델 생성

--- makourse/account/models.py
+++ makourse/account/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils.crypto import get_random_string # 초대링크 생성
 
-# Create your models here.
+# 사용자
+class User(models.Model):
+    id = models.CharField(max_length=100, unique=True, primary_key=True) # id
+    password = models.CharField(max_length=100) # pwd
+    name = models.CharField(max_length=30) # 이름
+    profile_image = models.ImageField(upload_to='user_photo/') # 프로필 사진
+    field = models.BooleanField(default=False) # 이용약관 동의 여부
+
+    def __str__(self): 
+        return self.id
+
+class UserGroup(models.Model):
+    code = models.CharField(max_length=30, unique=True, default=get_random_string)
+    
+    def generate_code(self): # 초대링크 생성 함수
+        self.code = get_random_string(length=30)
+        self.save()
+
+    def __str__(self):
+        self.code
+
+class GroupMembership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(UserGroup, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'group')  # 동일한 사용자가 같은 그룹에 중복 가입하지 않도록
+
+    def __str__(self):
+        return f"{self.user.name} in group {self.group.code}"
+
+# user와 group은 다대다 관계인듯
+# 한 user가 여러 그룹에 들어갈 수 있고, 한 group 안에도 여러 유저가 있을 수 있으니까
+# 그래서 GroupMembership으로 이어주는걸로 짬
--- makourse/course/models.py
+++ makourse/course/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from account.models import *
 
-# Create your models here.
+class MyPlace(models.Model): # 나만의 장소
+    place_name = models.CharField(max_length=10)
+
+    latitude = models.FloatField(default=0.0)  # 위도
+    longitude = models.FloatField(default=0.0)  # 경도
+    # 일단 위도, 경도를 사용할거 같아서 주소 string 필드는 안적음
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # 유저가 삭제되면 나만의 장소도 삭제
+
+    def __str__(self):
+        return self.place_name
+
+
+class Schedule(models.Model): # 일정(코스)
+    group = models.OneToOneField(UserGroup, on_delete=models.CASCADE, related_name='schedule')  # 일대일 관계 설정
+    meet_date = models.DateField(null=True) # 만나는 날짜(최대 3개로 설정하게 하려면 조금 복잡해서 일단 이렇게 해놓음..)
+    course_name = models.CharField(max_length=20, null=True)
+    meet_place = models.CharField(max_length=50, null=True)
+    latitude = models.FloatField(default=0.0)  # 만나는 장소의 위도
+    longitude = models.FloatField(default=0.0)  # 만나는 장소의 경도
+
+    def __str__(self):
+        return self.course_name
+
+
+class SeheduleEntry(models.Model): # 각 코스의 일정들
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE) # 코스의 외래키
+
+    num = models.IntegerField(default=0) # 순번이 차례로 증가하게 해야되는지 아니면 나중에 결정해야하는지..
+    entry_name = models.CharField(max_length=10) # 일정의 이름
+    time = models.TimeField() # 그 일정의 시간
+
+    open_time = models.TimeField() # 오픈 시간
+    close_time = models.TimeField() # 마감시간
+    # 운영시간은 이렇게 오픈, 마감 시간으로 따로 저장
+
+    # category는 지도 api에서 가져오는 거니까 모델에 따로 필요가 없나..?
+
+    content = models.TextField(null=True) # 메모
+
+    latitude = models.FloatField(default=0.0)  # 위도
+    longitude = models.FloatField(default=0.0)  # 경도
+
+    def __str__(self):
+        return self.entry_name
+
+class AlternativePlace(models.Model): # 대안장소
+    schedule_entry = models.ForeignKey(SeheduleEntry, on_delete=models.CASCADE)
+
+    latitude = models.FloatField(default=0.0)  # 위도
+    longitude = models.FloatField(default=0.0)  # 경도
+
+    name = models.CharField(max_length=10) # 대안장소 이름
+
+    # 여기 category도 위 모델과 같은 이유로 일단 안적음
