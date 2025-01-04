8f70f970c405280788aea10a67f3517b87e62297
bmh7190 <bmh7190@naver.com>
2025-01-04 23:43:01 +0900

    fix: 모임 삭제하면 일정까지 삭제되는 오류 수정정

--- makourse/course/models.py
+++ makourse/course/models.py
@@ -16,7 +16,7 @@ class MyPlace(models.Model): # 나만의 장소
 
 
 class Schedule(models.Model): # 일정(코스)
-    group = models.OneToOneField(UserGroup, on_delete=models.CASCADE, related_name='schedule', null=True, blank= True)  # 일대일 관계 설정
+    group = models.OneToOneField(UserGroup, on_delete=models.SET_NULL, related_name='schedule', null=True, blank= True)  # 일대일 관계 설정
     meet_date_first = models.DateTimeField(null=True, blank= True) # 만나는 날짜(최대 3개로 설정하게 하려면 조금 복잡해서 일단 이렇게 해놓음..)
     meet_date_second = models.DateTimeField(null=True, blank= True) # 2024-12-10T15:30:00 식으로 json 받기
     meet_date_third = models.DateTimeField(null=True , blank= True)
