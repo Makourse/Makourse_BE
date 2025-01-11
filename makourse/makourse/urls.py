6046df1657acdca5db383fe6848a250d3171eae5
Ubuntu <ubuntu@ip-172-31-44-6.ap-northeast-2.compute.internal>
2025-01-11 13:03:48 +0000

    fix: 메인 url에 media 경로 추가

--- makourse/makourse/urls.py
+++ makourse/makourse/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('course/', include('course.urls')),
     path('account/',include('account.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
