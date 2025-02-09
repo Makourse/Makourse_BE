40eb6e0efc3050f129f4b9e37a4254f735fbb863
dasol <5dasoll37@gmail.com>
2025-02-09 23:52:46 +0900

    Fix: url패턴 통일

--- makourse/account/urls.py
+++ makourse/account/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('<str:provider>/login/', SocialLoginAPIView.as_view(), name='social-login'), # 소셜 로그인
     path('<str:provider>/callback/', SocialLoginAPIView.as_view(), name='social-login-redirect'), # 소셜 로그인
     path('logout/', LogoutAPIView.as_view(), name='logout'), # 로그아웃
-    path('profile-image/update/', ProfileImageUpdateAPIView.as_view(), name='profile-image-update'), # 프로필 사진 업로드
-    path('profile-image/reset/', ResetProfileImageAPIView.as_view(), name='profile-image-reset'), # 프로필 기본 이미지로 변경
+    path('profile-image/update', ProfileImageUpdateAPIView.as_view(), name='profile-image-update'), # 프로필 사진 업로드
+    path('profile-image/reset', ResetProfileImageAPIView.as_view(), name='profile-image-reset'), # 프로필 기본 이미지로 변경
 
     # 유저에 따른 schedule 정보 보기
     path('schedules', UserSchedulesView.as_view(), name='user-schedules'),
@@ -26,6 +26,5 @@ urlpatterns = [
 
     # access token 재발급
     path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
-
 
 ]
--- makourse/course/urls.py
+++ makourse/course/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('myplace/<int:pk>', MyPlaceView.as_view()), # 나만의 장소 추가(post), 목록조회(get)
-    path('myplace/<int:myplace_id>/', MyPlaceDetailView.as_view()), # 나만의 장소 삭제(delete), 수정(patch)
+    path('myplace/<int:myplace_id>', MyPlaceDetailView.as_view()), # 나만의 장소 삭제(delete), 수정(patch)
     
     # 세부 일정
     path('schedule-entries/post/<int:schedule_id>', ScheduleEntryView.as_view(), name='schedule-entry-create'),  # POST
@@ -15,8 +15,8 @@ urlpatterns = [
     path('schedule-entries/<int:schedule_entry_id>/alternative-places/<int:alternative_place_id>/replace', ReplaceWithAlternativePlaceView.as_view(), name='replace-with-alternative-place'),
 
     # 일정(코스)
-    path('schedule/', ScheduleUpdateView.as_view()), # 일정(코스) 등록(post)
-    path('schedule/<int:schedule_id>/', ScheduleDetailView.as_view()), # 일정(코스) 상세조회(get), 수정(patch), 삭제(delete)
+    path('schedule', ScheduleUpdateView.as_view()), # 일정(코스) 등록(post)
+    path('schedule/<int:schedule_id>', ScheduleDetailView.as_view()), # 일정(코스) 상세조회(get), 수정(patch), 삭제(delete)
 
 
     # 자동 배포 확인용 api
